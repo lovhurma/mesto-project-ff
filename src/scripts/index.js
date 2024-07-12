eb5d222aa787e70ae85e821b20e56e1f97eb343b
Victoria Bochulya <v.bochulya@yandex.ru>
2024-07-12 19:35:37 +0300

    На этапе закрытия попап через оверлэй

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -38,3 +38,66 @@ function cardDelete(event) {
 }
 
 // Модальное окно
+
+//кнопки открытия модального окна
+const profilEeditBtn = document.querySelector('.profile__edit-button')
+const profilEddBtn = document.querySelector('.profile__add-button')
+
+//модальные окна
+const profelPopupEdit = document.querySelector('.popup_type_edit')
+const popupTypeNewCard = document.querySelector('.popup_type_new-card')
+
+//оверлэй
+const modalWondow = document.querySelector('.popup')
+
+//кнопка закрытия модального окна
+const popupCloseBtn = document.querySelectorAll('.popup__close')
+
+// Функция открытия модального окна
+const openPopup = (popup) => {
+  popup.classList.add('popup_is-opened')
+  document.addEventListener('keydown', handleCloseEsc)
+  document.addEventListener('click', modalWindowClose)
+}
+
+// Функция закрытия модального окна
+const closePopup = (popup) => {
+  popup.classList.remove('popup_is-opened')
+  document.removeEventListener('keydown', handleCloseEsc)
+  document.removeEventListener('click', modalWindowClose)
+
+}
+
+// Обработчики открытия модалки
+profilEeditBtn.addEventListener ('click', () => {
+  openPopup(profelPopupEdit)
+})
+
+profilEddBtn.addEventListener ('click', () => {
+  openPopup(popupTypeNewCard)
+})
+
+// Обработчик закрытия по кнопке (крестику)
+popupCloseBtn.forEach(item => {
+  item.addEventListener('click', () => {
+    closePopup(profelPopupEdit);
+    closePopup(popupTypeNewCard);
+  });
+});
+
+//Функция закрытия Escape
+const handleCloseEsc = (evt) => {
+  if (evt.key === 'Escape') {
+    const popupActive = document.querySelector('.popup_is-opened')
+    closePopup(popupActive)
+  }
+}
+
+const modalWindowClose = (evt) => {
+  if (evt.target === modalWondow) {
+    closePopup(modalWondow)
+  }
+}
+
+
+
