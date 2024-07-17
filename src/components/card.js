e7c1b2f89f2bad58a47a48e6fd1bdc5a26371036
Victoria Bochulya <v.bochulya@yandex.ru>
2024-07-17 10:13:52 +0300

    1 сдача проекта

---
+++ src/components/card.js
@@ -0,0 +1,39 @@
+const cardTemplate = document.querySelector('#card-template').content
+
+// @todo: Функция создания карточки
+function createCard(element, cardDelete, onLikeFnc, OpenImageClick) {
+  const placeTemplate = cardTemplate.querySelector('.places__item').cloneNode(true)
+  
+  const cardImage = placeTemplate.querySelector('.card__image')
+  const cardTitle = placeTemplate.querySelector('.card__title')
+  const deleteCard = placeTemplate.querySelector('.card__delete-button')
+  const cardLikeBtn = placeTemplate .querySelector('.card__like-button')
+  
+  cardImage.src = element.link;
+  cardTitle.textContent = element.name;
+  cardImage.alt = `Изображение ${element.name}`
+  
+  cardImage.addEventListener('click', () => {
+    OpenImageClick(element);
+  });
+  
+  deleteCard.addEventListener('click', cardDelete) 
+  cardLikeBtn.addEventListener('click', onLikeFnc)
+  
+  return placeTemplate
+  }
+
+// @todo: Функция удаления карточки
+
+function cardDelete(event) {
+  let card = event.target.closest('.card')
+  card.remove()
+}
+
+//Функия лайка
+
+function onLikeFnc(evt) {
+  evt.target.classList.toggle('card__like-button_is-active')
+}
+
+export {createCard, cardDelete, onLikeFnc}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,60 +1,25 @@
 import '../pages/index.css';
 import { initialCards } from './cards.js'
+import { createCard, cardDelete, onLikeFnc } from '../components/card.js'
+import { openPopup, closePopup } from '../components/modal.js'
 
-
-const cardTemplate = document.querySelector('#card-template').content
 const cardPlaceList = document.querySelector('.places__list')
-const likeBtn = document.querySelectorAll('.card__like-button')
-
-//кнопки открытия модального окна
 const profilEeditBtn = document.querySelector('.profile__edit-button')
 const profilEddBtn = document.querySelector('.profile__add-button')
-
-//модальные окна
 const profelPopupEdit = document.querySelector('.popup_type_edit')
 const popupTypeNewCard = document.querySelector('.popup_type_new-card')
 const popupTypeImage = document.querySelector('.popup_type_image')
 const popupImage = document.querySelector('.popup__image')
 const popupCuption = document.querySelector('.popup__caption')
-
-//кнопка закрытия модального окна
 const popupCloseBtn = document.querySelectorAll('.popup__close')
-
-//Получение полей описания профиля
 const profileTitle = document.querySelector('.profile__title')
 const profileDescription = document.querySelector('.profile__description')
-
-// Находим форму в DOM
 const formElement = document.forms['edit-profile']
 const formNewCardElement = document.forms['new-place']
-// Находим поля формы в DOM
 const nameInput = document.querySelector('.popup__input_type_name')
 const jobInput = document.querySelector('.popup__input_type_description')
 const cardNameInput = document.querySelector('.popup__input_type_card-name')
 const cardInpurUrl = document.querySelector('.popup__input_type_url')
-
-// @todo: Функция создания карточки
-function createCard(element, cardDelete, onLikeFnc, OpenImageClick) {
-const placeTemplate = cardTemplate.querySelector('.places__item').cloneNode(true)
-
-const cardImage = placeTemplate.querySelector('.card__image')
-const cardTitle = placeTemplate.querySelector('.card__title')
-const deleteCard = placeTemplate.querySelector('.card__delete-button')
-const cardLikeBtn = placeTemplate .querySelector('.card__like-button')
-
-cardImage.src = element.link;
-cardTitle.textContent = element.name;
-cardImage.alt = `Изображение ${element.name}`
-
-cardImage.addEventListener('click', () => {
-  OpenImageClick(element);
-});
-
-deleteCard.addEventListener('click', cardDelete) 
-cardLikeBtn.addEventListener('click', onLikeFnc)
-
-return placeTemplate
-}
 
 // @todo: Вывести карточки на страницу
 
@@ -63,42 +28,12 @@ initialCards.forEach(function (element) {
   cardPlaceList.append(newCard)
 })
 
-// @todo: Функция удаления карточки
-
-function cardDelete(event) {
-  let card = event.target.closest('.card')
-  card.remove()
-}
-
-//Функия лайка
-
-function onLikeFnc(evt) {
-  evt.target.classList.toggle('card__like-button_is-active')
-}
-
 //Функция открытия попапа с картинкой
 function OpenImageClick(item) {
   popupImage.src = item.link
   popupImage.alt = item.name
   popupCuption.textContent = item.name
   openPopup(popupTypeImage)
-}
-
-// Функция открытия модального окна
-const openPopup = (popup) => {
-  popup.classList.add('popup_is-opened')
-  popup.classList.add('popup_is-animated')
-  document.addEventListener('keydown', handleCloseEsc)
-  popup.addEventListener('click', modalWindowClose)
-
-}
-
-// Функция закрытия модального окна
-const closePopup = (popup) => {
-  popup.classList.remove('popup_is-opened')
-  document.removeEventListener('keydown', handleCloseEsc)
-  popup.removeEventListener('click', modalWindowClose)
-
 }
 
 // Обработчик открытия модалки редактирования профиля
@@ -157,19 +92,6 @@ popupCloseBtn.forEach(item => {
   });
 });
 
-//Функция закрытия Escape
-const handleCloseEsc = (evt) => {
-  if (evt.key === 'Escape') {
-    const popupActive = document.querySelector('.popup_is-opened')
-    closePopup(popupActive)
-  }
-}
-
-const modalWindowClose = (evt) => {
-  if (evt.target === evt.currentTarget) {
-    closePopup(evt.currentTarget);
-  };
-};
 
 
 
